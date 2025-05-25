11673547f1264190ad53162977d5279a4d40b555
BiezinDaAgua <gabriel_oliveira_jp@hotmail.com>
2025-05-25 17:34:16 -0300

    adicionando firebase

---
+++ src/main/java/br/edu/ifpb/GastoZen/config/FirebaseConfig.java
@@ -0,0 +1,33 @@
+package br.edu.ifpb.GastoZen.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    private static final String CREDENTIALS_JSON = "gastozen-firebase-adminsdk.json";
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        InputStream serviceAccount =
+                new ClassPathResource(CREDENTIALS_JSON).getInputStream();
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                // .setDatabaseUrl("https://<SEU-PROJETO>.firebaseio.com") // opcional
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            return FirebaseApp.initializeApp(options);
+        }
+        return FirebaseApp.getInstance();
+    }
+}
