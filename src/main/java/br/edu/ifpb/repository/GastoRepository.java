514f32fc517e4b1ec1d5b9102675fd797898faf8
Arthur Lyra Miranda <arthur.miranda@compasso.com.br>
2025-05-27 13:53:43 -0300

    feat: criação do repositorio de gastos

---
+++ src/main/java/br/edu/ifpb/repository/GastoRepository.java
@@ -0,0 +1,35 @@
+package br.edu.ifpb.repository;
+
+import br.edu.ifpb.model.Gasto;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.api.core.ApiFuture;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+public class GastoRepository {
+    private final Firestore firestore;
+    private static final String COLLECTION_NAME = "gastos";
+
+    public GastoRepository(Firestore firestore) {
+        this.firestore = firestore;
+    }
+
+    public Gasto save(Gasto gasto) throws ExecutionException, InterruptedException {
+        DocumentReference docRef = firestore.collection(COLLECTION_NAME).document();
+        ApiFuture<Void> future = docRef.set(gasto);
+        future.get(); // Wait for the operation to complete
+        gasto.setId(Long.parseLong(docRef.getId()));
+        return gasto;
+    }
+
+    public List<Gasto> findAll() throws ExecutionException, InterruptedException {
+        ApiFuture<QuerySnapshot> future = firestore.collection(COLLECTION_NAME).get();
+        QuerySnapshot querySnapshot = future.get();
+        return querySnapshot.getDocuments().stream()
+                .map(document -> document.toObject(Gasto.class))
+                .collect(Collectors.toList());
+    }
+}
