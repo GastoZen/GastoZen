1b594687db9a634595c01e70df531ddffe5e1ae1
Arthur Lyra Miranda <arthur.miranda@compasso.com.br>
2025-05-27 13:23:52 -0300

    refact: pequena modificação

--- src/main/java/br/edu/ifpb/GastoZen/security/SecurityConfig.java
+++ src/main/java/br/edu/ifpb/GastoZen/security/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/login").permitAll()
                         .requestMatchers("/api/publico/**").permitAll()
                         .requestMatchers("/api/protegido/**").authenticated()
                         .anyRequest().authenticated())
